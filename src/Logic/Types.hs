af99d42ddff5820d942f63df8a64f69c0bb11d95
Bartek Banachewicz <bananu7@o2.pl>
2015-12-21 23:08:15 +0100

    Fixed some terrible holes in the types idea

--- src/Logic/Types.hs
+++ src/Logic/Types.hs
@@ -8,9 +8,15 @@ import Control.Lens
 import qualified Data.Map as Map
 import Control.Monad.State.Class
 
+-- | Id is something that tags an instance of something
 type Id = Int
+
 newtype PlayerId = PlayerId Id
 newtype UnitId = UnitId Id
+
+-- | Kind tags a static type of something
+type Kind = Int
+newtype BuildingKind = BuildingKind Kind
 
 data UnitState = Idle | Moving | Harvesting | Fighting
 
@@ -23,10 +29,16 @@ data Building = Building {
     _buildingMaxHp :: Int
     }
 
+data PlayerState = PlayerState {
+    _gold :: Int,
+    _wood :: Int
+    }
+
 -- | This is a complete snapshot of a game.
 data GameState = GameState {
     _units :: Map.Map UnitId Unit,
-    _tick :: Int -- | this is the tick number of the current game
+    _tick :: Int, -- | this is the tick number of the current game
+    _players :: Map.Map PlayerId PlayerState
     }
 
 -- | Universal type for stateful contexts.
@@ -38,12 +50,14 @@ data Position = Position Int Int
 -- | Represents a position used on the grid by building units.
 data GridPosition = GridPosition Int Int
 
-
--- | Represents a possible
+-- | Represents a possible target for a command
 data Target = PositionTarget Position | UnitTarget UnitId
 
 -- | One atomic input coming from a player.
-data Command = MoveCommand UnitId Target | AttackCommand 
+data Command =
+    MoveCommand UnitId Target |
+    AttackCommand UnitId Target |
+    BuildCommand UnitId BuildingKind GridPosition
 
 -- | Represents input coming from a player.
 -- A player can submit more than one command for each tick.
